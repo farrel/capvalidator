2cdabf9501c0775102e2fdd83e6151e89de54da6
Farrel Lifson <farrel.lifson@aimred.com>
2011-04-03 03:15:42 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "http://rubygems.org"
+
+gem "sinatra"
+gem "rcap", "1.0.0"
+gem "haml"
+gem "coderay"
---
+++ validator.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+require 'sinatra'
+require 'rcap'
+require 'haml'
+require 'coderay'
+require 'rexml/formatters/pretty'
+
+XML_FORMATTER = REXML::Formatters::Pretty.new( 2 )
+XML_FORMATTER.compact = true
+
+get '/' do
+  haml( :index )
+end
+
+
+post '/validate' do
+  @alert = RCAP::Alert.from_xml( params[ :cap_data ]) rescue RCAP::CAP_1_2::Alert.new
+  if @alert.valid?
+    @xml_string = ""
+    XML_FORMATTER.write( @alert.to_xml_document.root, @xml_string ) 
+    @yaml_string = @alert.to_yaml
+    @json_string = JSON.pretty_generate( @alert.to_h )
+  end
+  haml( :validate )
+end
