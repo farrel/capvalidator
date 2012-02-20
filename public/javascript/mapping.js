f2d63e4bf2c7a5defa70c463da1363ac111133df
Farrel Lifson <farrel.lifson@aimred.com>
2012-02-20 21:29:11 +0200

    Refactoring

--- public/javascript/mapping.js
+++ public/javascript/mapping.js
@@ -12,6 +12,19 @@ function createPolygonFeature( polygonName, polygonCoords ) {
   return new OpenLayers.Feature.Vector( polygon.transform( fromProjection, toProjection )); 
 }
 
+function addPolygonsToMap( map, polygonsData ) {
+  var polygons = new OpenLayers.Layer.Vector( "Polygons" );
+  map.addLayer( polygons );
+
+  $.each( polygonsData, function( i, polygonData ){
+    polygons.addFeatures( $.map( polygonData[ "polygons" ], function( polygonCoords, j ){
+      return createPolygonFeature( polygonData[ "description" ], polygonCoords );
+    }));
+  });
+
+  return polygons;
+}
+
 function createCircleFeature( polygonName, circleCoords ) {
   var origin = new OpenLayers.Geometry.Point( circleCoords[ 0 ], circleCoords[ 1 ]);
 
@@ -19,4 +32,41 @@ function createCircleFeature( polygonName, circleCoords ) {
   circle.calculateBounds();
   
   return new OpenLayers.Feature.Vector( circle.transform( fromProjection, toProjection )); 
+}
+
+function addCirclesToMap( map, circlesData ){
+  var circles = new OpenLayers.Layer.Vector( "Circles" );
+  map.addLayer( circles );
+
+  $.each( circlesData, function( i, circleData ){
+    circles.addFeatures( $.map( circleData[ "circles" ], function( circleCoords, j ){
+      return createCircleFeature( circleData[ "description" ], circleCoords );
+    }));
+  });
+
+  return circles;
+}
+
+function init_map(){ 
+  var map = new OpenLayers.Map('map',{
+    units: 'm',
+    projection: fromProjection,
+    displayProjection: toProjection });
+  var osm = new OpenLayers.Layer.OSM();
+  map.addLayer( osm );
+
+  return map;
+}
+
+function build_map(){
+  var ol_map = init_map();
+  addPolygonsToMap( ol_map, polygonsData );
+  addCirclesToMap( ol_map, circlesData );
+
+  var bounds = new OpenLayers.Bounds();
+  $.each( ol_map.layers, function(i, layer ){
+    bounds.extend( layer.getDataExtent());
+  });
+  ol_map.zoomToExtent( bounds );
+  return ol_map;
 }
